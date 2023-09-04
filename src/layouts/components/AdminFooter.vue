75760a30da2065000e2ff6f973efb18be9e84942
Tim_Brown <82385261+Albus-Tim-Brown@users.noreply.github.com>
2023-09-04 11:38:57 +0800

    first commit

---
+++ src/layouts/components/AdminFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        Copyright © 2023. All rights reserved. Provided by&nbsp;<a class="tim" href="https://albus-tim-brown.gitee.io" target="_blank">Tim_Brown</a>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    @apply text-gray-500 text-sm justify-center flex;
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    background-color: #fff;
+    margin-top: 5rem;
+}
+
+.tim {
+    text-decoration: underline;
+    color: rgb(52, 152, 219);
+}
+</style>
